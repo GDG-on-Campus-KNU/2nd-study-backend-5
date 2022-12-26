61857acc346dff1ae0d4a31f0e61e6b131d4483f
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2022-12-26 18:27:02 +0900

    init spring

---
+++ Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/HappyNewYearApplication.java
@@ -0,0 +1,13 @@
+package GDSCBackend5.HappyNewYear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyNewYearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyNewYearApplication.class, args);
+	}
+
+}
---
+++ Happy-New-Year/src/test/java/GDSCBackend5/HappyNewYear/HappyNewYearApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSCBackend5.HappyNewYear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyNewYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
