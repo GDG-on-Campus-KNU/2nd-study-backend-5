3c316c60fe89adef52afd3d2859dfbad511161fb
kdozlo <eodud5730@naver.com>
2022-12-31 23:50:33 +0900

    fixed PostService class

--- Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/service/PostService.java
+++ Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/service/PostService.java
@@ -2,12 +2,8 @@ package GDSCBackend5.HappyNewYear.server.service;
 
 import GDSCBackend5.HappyNewYear.server.domain.Post;
 import GDSCBackend5.HappyNewYear.server.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Date;
 
 @Service
 public class PostService {
