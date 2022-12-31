b92492195340df0e7de6acff59e492d680fcccb8
kdozlo <eodud5730@naver.com>
2022-12-31 23:49:34 +0900

    fixed PostService class and create PostServiceTest

--- Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/service/PostService.java
+++ Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/service/PostService.java
@@ -2,8 +2,12 @@ package GDSCBackend5.HappyNewYear.server.service;
 
 import GDSCBackend5.HappyNewYear.server.domain.Post;
 import GDSCBackend5.HappyNewYear.server.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 public class PostService {
@@ -11,7 +15,9 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-    public void write(Post post){
+    public Long write(Post post){
         postRepository.save(post);
+
+        return post.getPostid();
     }
 }
