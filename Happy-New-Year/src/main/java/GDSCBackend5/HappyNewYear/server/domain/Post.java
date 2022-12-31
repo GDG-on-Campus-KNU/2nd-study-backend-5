8002ec74c8fb297e5c39350fbd361114c57592b7
kdozlo <eodud5730@naver.com>
2022-12-31 17:06:47 +0900

    change Post class

--- Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/domain/Post.java
+++ Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/domain/Post.java
@@ -1,8 +1,20 @@
 package GDSCBackend5.HappyNewYear.server.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
 import java.util.Date;
 
+@Entity
+@Data
 public class Post {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "POST_ID")
     private Long postid;
 
     private String sender;
@@ -15,52 +27,5 @@ public class Post {
 
     private Date regdate;
 
-    public Long getPostid() {
-        return postid;
-    }
-
-    public void setPostid(Long postid) {
-        this.postid = postid;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getRegdate() {
-        return regdate;
-    }
-
-    public void setRegdate(Date regdate) {
-        this.regdate = regdate;
-    }
 
 }
