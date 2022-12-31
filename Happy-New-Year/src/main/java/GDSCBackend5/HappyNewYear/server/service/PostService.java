f23783b0c67561a7c3f7e34e9659779d89fb3224
kdozlo <eodud5730@naver.com>
2022-12-31 18:57:46 +0900

    create PostService class

---
+++ Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/service/PostService.java
@@ -0,0 +1,17 @@
+package GDSCBackend5.HappyNewYear.server.service;
+
+import GDSCBackend5.HappyNewYear.server.domain.Post;
+import GDSCBackend5.HappyNewYear.server.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public void write(Post post){
+        postRepository.save(post);
+    }
+}
