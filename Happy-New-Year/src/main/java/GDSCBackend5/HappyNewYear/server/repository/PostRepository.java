17fdcc0ea95887c24efe395c7a89cb36ed6cea82
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-01-02 22:15:51 +0900

    make findByToken

--- Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/repository/PostRepository.java
+++ Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/repository/PostRepository.java
@@ -4,6 +4,8 @@ import GDSCBackend5.HappyNewYear.server.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByToken(String token);
 }
