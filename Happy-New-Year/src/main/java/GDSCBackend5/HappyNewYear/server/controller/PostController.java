5f2b87ce0fd08f94a67c308ebd67e42fe6b6869a
kdozlo <eodud5730@naver.com>
2023-01-05 15:49:18 +0900

    update writingHome.html

--- Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/controller/PostController.java
+++ Happy-New-Year/src/main/java/GDSCBackend5/HappyNewYear/server/controller/PostController.java
@@ -4,6 +4,7 @@ import GDSCBackend5.HappyNewYear.server.domain.Post;
 import GDSCBackend5.HappyNewYear.server.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,7 +14,8 @@ public class PostController {
     private PostService postService;
 
     @GetMapping("/writinghome")
-    public String writingHome() {
+    public String writingHome(Model model) {
+        model.addAttribute("name", "kdozlo"+"님의 복주머니");
         return "writingHome";
     }
 
